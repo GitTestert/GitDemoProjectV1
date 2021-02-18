ee8f9443c13352d7e2196006f73e84be0353e09c
GitTestert <mamatha.konda.qa@gmail.com>
2021-02-18 15:31:05 +0530

    First Commit

---
+++ src/Test1.java
@@ -0,0 +1,13 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is sample demo project to explain git and github
+		
+		System.out.println("This is done by lead initiation project");
+
+	}
+
+}
